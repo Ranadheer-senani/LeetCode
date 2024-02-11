53ff9946a4e82ef2cb62f4b70929924d13e9e253
Ranadheer Senani Dasari <57611882+Ranadheer-senani@users.noreply.github.com>
2024-02-12 01:21:58 +0530

    Time: 121 ms (98.35%) | Memory: 18.1 MB (63.51%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,10 +2,11 @@ class Solution:
     def majorityElement(self, a: List[int]) -> int:
         cand  = a[0]
         votes = 0
+        z = len(a)//2
         for i in a:
             if votes == 0:
                 cand = i
             votes += 1 if cand == i else -1
-            if votes > len(a)//2:
+            if votes > z:
                 return cand
         return cand
