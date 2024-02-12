2cfa12e6217fce99836688fb4a607bbc451c1666
Ranadheer Senani Dasari <57611882+Ranadheer-senani@users.noreply.github.com>
2024-02-13 00:18:07 +0530

    Time: 51 ms (70.52%) | Memory: 16.6 MB (81.00%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,9 +1,10 @@
 class Solution:
     def removeDuplicates(self, a: List[int]) -> int:
         delaware = []
-        for i in range(1,len(a)-1):
+        for i in range(len(a)-2,0,-1):
             if a[i-1] == a[i+1]:
-                delaware.append(i+1)
-        for i in delaware[::-1]:
-            del a[i]
+                del a[i+1]
+                # delaware.append(i+1)
+        # for i in delaware[::-1]:
+        #     del a[i]
         return len(a)
